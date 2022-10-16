93beded1b86363247fee2a16221eb4f337bc86e5
Amor1205 <453177150@qq.com>
2022-10-16 14:12:00 +0800

    class and many types of function

--- C++/lesson_2/test.cpp
+++ C++/lesson_2/test.cpp
@@ -107,36 +107,144 @@ using namespace std;
 //     return 0;
 // }
 
+// class Date
+// {
+// public:
+//     void Init(int year, int month, int date)
+//     {
+//         // Date::year = year
+//         _year = year;
+//         _month = month;
+//         _date = date;
+//     }
+//     void Print()
+//     {
+//         cout << _year << "-" << _month << "-" << _date << endl;
+//     }
+
+// private:
+//     // int year;//如果不加_，需要用Date::和函数中形参区分。
+//     int _year; //加_就是为了区分函数中的形参和实参
+//     int _month;
+//     int _date;
+// };
+
+// int main()
+// {
+//     Date d1;
+//     Date d2;
+//     d1.Init(2022, 10, 15);
+//     d1.Print();
+//     d2.Init(2022, 12, 15);
+//     d2.Print();
+//     return 0;
+// }
+// d1.Print(&d1), d2.Print(&d2);
+
+// class A
+// {
+// public:
+//     void Show()
+//     {
+//         cout << "Show()" << endl;
+//     }
+
+// private:
+//     int _a;
+// };
+// class B
+// {
+// public:
+//     void Show()
+//     {
+//         cout << "_b" << endl;
+//     }
+
+// private:
+//     int _b;
+// };
+// int main()
+// {
+//     A *pa = nullptr;
+//     B *pb = nullptr;
+
+//     pa->Show();
+//     pb->Show();
+// }
+
+// int main()
+// {
+//     Date d1;               //无参数初始化d1
+//     Date d2(2022, 10, 16); //有参数初始化d2
+//     return 0;
+// }
+class Stack
+{
+public:
+    Stack(size_t capacity = 4)
+    {
+        _a = (int *)malloc(sizeof(int) * capacity);
+        if(_a == nullptr)
+        {
+            perror("malloc");
+            exit(-1);
+        }
+        _top = 0;
+        _capacity = capacity;
+    }
+    ~Stack()
+    {
+        free(_a);
+        _a = nullptr;
+        _top = _capacity = 0;
+    }
+
+private:
+    int *_a;
+    size_t _capacity;
+    size_t _top;
+};
+
+
+// int main()
+// {
+//     Date d1;               //无参数初始化d1
+//     Date d2(2022, 10, 16); //有参数初始化d2
+
+//     Stack s1;
+//     Stack s2(10);
+
+//     return 0;
+// }
+
 class Date
 {
 public:
-    void Init(int year, int month, int date)
+    Date(int year = 0, int month = 1, int date = 1)
     {
-        // Date::year = year
         _year = year;
         _month = month;
         _date = date;
     }
-    void Print()
+    ~Date()
     {
-        cout << _year << "-" << _month << "-" << _date << endl;
+    } // Date类没有资源需要清理，所以Date不实现析构函数都是可以的。
+    Date(Date& d)
+    {
+        _year = d._year;
+        _month = d._month;
+        _date = d._date;
     }
 
 private:
-    // int year;//如果不加_，需要用Date::和函数中形参区分。
-    int _year; //加_就是为了区分函数中的形参和实参
+    int _year;
     int _month;
     int _date;
 };
 
 int main()
 {
-    Date d1;
-    Date d2;
-    d1.Init(2022, 10, 15);
-    d1.Print();
-    d2.Init(2022, 12, 15);
-    d2.Print();
+    Date d1(2022,10,16);
+    Date d2(d1);
     return 0;
 }
-// d1.Print(&d1), d2.Print(&d2);
