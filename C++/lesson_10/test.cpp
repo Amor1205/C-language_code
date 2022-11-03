cb0a6c4e95d8487691659cd6c50aa5f2c20c6001
Amor1205 <453177150@qq.com>
2022-11-03 22:49:29 +0800

    pq

---
+++ C++/lesson_10/test.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <functional>
+#include "priority_queue.cpp"
+
+using namespace amor;
+
+// void test_topk1(vector<int>& nums, int k)
+//{
+//	sort(nums.begin(),nums.end(),greater<int>());
+//	return nums[k-1];
+// }
+// O(n*logn)
+// void test_topk2(vector<int>& nums, int k)
+//{
+//	priority_queue<int> manHeap(nums.begin(),nums.end());
+//	while(--k)//pop k-1 times
+//	{
+//		maxHeap.pop();
+//	}
+//	return manHeap.top();
+// }
+// O(N+K*logN)
+// void test_topk3(vector<int>& nums, int k)
+//{
+//	priority_queue<int, vector<int>, greater<int>> minHeap(nums.begin(),nums.begin()+k);
+//	for(size_t i = k;i<nums.size();++i)
+//	{
+//		if(nums[i]>minHeap.top())
+//		{
+//			minHeap.pop();
+//			minHeap.push(nums[i]);
+//		}
+//	}
+//	return minHeap.top();
+// }
+// O(K+(N-K)*logK) --> O(N*logK)
+// the best : 3
+
+void test_priority_queue()
+{
+    amor::priority_queue<int, std::vector<int>> pq;
+    pq.push(3);
+    pq.push(4);
+    pq.push(5);
+    pq.push(2);
+    pq.push(1);
+    pq.push(7);
+    while (!pq.empty())
+    {
+        std::cout << pq.top() << std::endl;
+        pq.pop();
+    }
+}
+
+int main()
+{
+    test_priority_queue();
+    return 0;
+}
